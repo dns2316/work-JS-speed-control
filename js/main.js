6c2e26913f54f9dae95f4e2c4b4b0645b157e94b
dns2316 <dns2316@gmail.com>
2017-03-23 17:46:51 +0200

    add gallery, wait pay

--- js/main.js
+++ js/main.js
@@ -13,6 +13,24 @@ $( document ).ready(function() {
 	});
 });
 
+/*
+$(window).scroll(function() {
+  var windTop = $(window).scrollTop();
+  var splashHeight = $(".splash").height();
+
+  if(windTop>(100)){
+  	 $(window).off("scroll");
+  	  $(".splash").slideUp("800", function() {
+	     $("html, body").animate({"scrollTop":"0px"},100);
+     });
+     $(".wrapper").animate({"opacity":"1.0"},800);
+  }
+  else {
+
+  }
+});
+*/
+
 $(window).scroll(function() {
   	  $(window).on("scroll");
 	  $(".splash").slideUp("800", function() {
@@ -26,9 +44,20 @@ $(window).scroll(function() {
 // js hover splash
 $(".cover").hide();
 $(".vdvd").mouseover(function(){
+	// $(".cover").show(500);
   $(".cover").show().animate({"opacity":"1.0"}, 500);
 });
 $(".vdvd").mouseleave(function(){
+	// $(".cover").hide(500);
   $(".cover").hide().animate({"opacity":"0.0"}, 500);
 });
 // end hover splash
+
+// Galleria.loadTheme('js/themes/classic/galleria.classic.min.js');
+// Galleria.run('.galleria');
+
+// $("#mysitelogo").click(
+//   function (e) {
+//     $('.galleria').hide()
+//   }
+// )
